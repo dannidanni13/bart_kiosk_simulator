942f5bb3d025969b1a57f075a09dfddb28ed511f
Your Name <you@example.com>
2016-05-02 20:31:24 -0700

    first commit

---
+++ bart_kiosk_simulator.py
@@ -0,0 +1,32 @@
+def load_card(bill1,bill5, bill10, bill20):
+	num1 = bill1*1
+	num5 = bill5*5
+	num10 = bill10*10
+	num20 = bill20*20
+	return num1+num5+num10+num20
+print load_card(0,0,0,0)
+
+def load_card(bill1,bill5, bill10, bill20):
+	num1 = bill1*1
+	num5 = bill5*5
+	num10 = bill10*10
+	num20 = bill20*20
+	return num1+num5+num10+num20
+print load_card(0,0,0,9)
+
+def load_card(bill1,bill5, bill10, bill20):
+	num1 = bill1*1
+	num5 = bill5*5
+	num10 = bill10*10
+	num20 = bill20*20
+	return num1+num5+num10+num20
+print load_card(2,3,0,0)
+
+def load_card(bill1,bill5, bill10, bill20):
+	num1 = bill1*1
+	num5 = bill5*5
+	num10 = bill10*10
+	num20 = bill20*20
+	return num1+num5+num10+num20
+print load_card(3,1,1,3)
+
